5be837fe9b78344d397f9f947f6416b3e9f2c1b4
Muhammad Ihsanul 'Arifin <72428799+ihsanularifinm@users.noreply.github.com>
2025-11-06 09:31:59 +0700

    Update page.tsx

--- frontend/src/app/admin/services/edit/[id]/page.tsx
+++ frontend/src/app/admin/services/edit/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } => 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useRouter, useParams } from 'next/navigation';
