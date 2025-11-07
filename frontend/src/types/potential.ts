fb451c1deb814f267994cb1f5ab3df2ec128d751
Muhammad Ihsanul 'Arifin <72428799+ihsanularifinm@users.noreply.github.com>
2025-11-07 10:53:39 +0700

    Update potential.ts

--- frontend/src/types/potential.ts
+++ frontend/src/types/potential.ts
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 
 export const schema = yup.object().shape({
   title: yup.string().required('Judul tidak boleh kosong'),
-  description: yup.string().optional(),
-  cover_image_url: yup.string().url('URL tidak valid').optional(),
+    description: yup.string().optional().default(''),
+    cover_image_url: yup.string().url('URL tidak valid').optional().default(''),
   type: yup.string().oneOf(['umkm', 'tourism', 'agriculture', 'other']).required('Tipe tidak boleh kosong'),
 });
 
