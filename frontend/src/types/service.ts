dd7634e649f67dee31ed407eeada92a8e5628ca6
Muhammad Ihsanul 'Arifin <72428799+ihsanularifinm@users.noreply.github.com>
2025-11-07 11:27:36 +0700

    Update service.ts

--- frontend/src/types/service.ts
+++ frontend/src/types/service.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const schema = yup.object().shape({
   service_name: yup.string().required('Nama layanan tidak boleh kosong'),
   description: yup.string().required('Deskripsi tidak boleh kosong'),
-  requirements: yup.string().optional(),
+    requirements: yup.string().optional().default(''),
 });
 
 export type ServiceFormData = yup.InferType<typeof schema>;
